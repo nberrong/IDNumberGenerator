f570c045cbb754f4d67739360a4c5b0756598fa1
Nikki Reichert <nberrong@qx.net>
2024-05-02 22:55:54 -0400

    check to see if maximum number of cards has been issued

--- script.js
+++ script.js
@@ -1,72 +1,83 @@
 const numberDigitsInID = 9;
-let clickNumber = 0;
+let cardNumber = 1;
 let generatedCards = [];
+let MAXNUMBEROFCARDS = Math.pow(10, numberDigitsInID);
 
 
 function getRandomInt() {
-    return Math.floor(Math.random() * 9);
+    return Math.floor(Math.random() * 10);
 }
   
 
 function generateCard() {
-    let newIdNumber = generateNumber();
-    const newCard = document.createElement("div");
-    const cardContainer = document.getElementById("card-container");
-  
-    newCard.className = "card";
+
+    console.log(cardNumber + " / " + MAXNUMBEROFCARDS);
+
+    if (cardNumber <= MAXNUMBEROFCARDS) {
+        let newIdNumber = generateNumber();
+        const newCard = document.createElement("div");
+        const cardContainer = document.getElementById("card-container");
     
-    clickNumber++;
+        newCard.className = "card";
+        
+        newCard.innerHTML = 
+            `<h2>Identification Card #${cardNumber}</h2>
+            <p>${newIdNumber}</p>`
+        
+        cardContainer.appendChild(newCard);
+        
+        generatedCards.push(newIdNumber);
+        cardNumber++;
     
-    newCard.innerHTML = `<h2>Identification Card #${clickNumber}</h2>
-    <p>${newIdNumber}</p>`
-    
-    cardContainer.appendChild(newCard);
-    
-    generatedCards.push(newIdNumber);
-    console.log(generatedCards);
+        console.log(generatedCards);
+    } else {
+        alert("Maximum number of cards issued!");
+        
+    }
 } 
   
     
 function generateNumber() {
+ 
     let digits = new Array(); 
     let numberString = "";
     let uniqueNumber = true;
   
-do {
-    uniqueNumber = true;
-   
-   for (let i = 0; i < numberDigitsInID; i++) {
-        let x = getRandomInt();
-        if (i === 3 || i === 5) {
-            digits.push('-');
-        }
-        digits.push(x);
-    }
-  
-    numberString = digits.join('');
+    do {
+        uniqueNumber = true;
     
-    // make sure new number is unique
-    for (let i=0; i < generatedCards.length; i++) {
-        if (numberString === generatedCards[i]) {
-            uniqueNumber = false;
-            digits = [];
-            console.log("duplicate:" + numberString);
+    for (let i = 0; i < numberDigitsInID; i++) {
+            let x = getRandomInt();
+            if (i === 3 || i === 5) {
+                digits.push('-');
+            }
+            digits.push(x);
         }
-    } 
-} while (uniqueNumber === false);
+    
+        numberString = digits.join('');
+        
+        // make sure new number is unique
+        for (let i=0; i < generatedCards.length; i++) {
+            if (numberString === generatedCards[i]) {
+                uniqueNumber = false;
+                digits = [];
+                console.log("duplicate:" + numberString);
+            }
+        } 
+    } while (uniqueNumber === false);
 
     return numberString;
 }
   
   
 function clearCards() {
-    clickNumber = 0;
+    cardNumber = 1;
     generatedCards = [];
    
     const cards = document.getElementById("card-container");
     cards.innerHTML = "";
   
-    console.log(clickNumber);
+    console.log(cardNumber);
     console.log(generatedCards);
 }
   
